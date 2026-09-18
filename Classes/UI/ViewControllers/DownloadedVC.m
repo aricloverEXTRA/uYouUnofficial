0981661f0256713072acc58ad372508a36f98349
aricloverEXTRA <157071384+aricloverEXTRA@users.noreply.github.com>
2026-09-17 20:01:37 -0500

    more fixes

--- Classes/UI/ViewControllers/DownloadedVC.m
+++ Classes/UI/ViewControllers/DownloadedVC.m
@@ -1,68 +1,7 @@
 #import "DownloadedVC.h"
 #import "DownloadsManager.h"
 #import "DownloadItem.h"
-
-@interface DownloadedCell : UITableViewCell
-@property (nonatomic, strong) UIImageView *thumbnailImageView;
-@property (nonatomic, strong) UILabel *titleLabel;
-@property (nonatomic, strong) UILabel *statusLabel;
-@property (nonatomic, strong) UIProgressView *progressView;
-@end
-
-@implementation DownloadedCell
-
-- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
-    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
-    if (self) {
-        self.backgroundColor = [UIColor systemBackgroundColor];
-        self.contentView.backgroundColor = [UIColor systemBackgroundColor];
-
-        _thumbnailImageView = [[UIImageView alloc] init];
-        _thumbnailImageView.contentMode = UIViewContentModeScaleAspectFill;
-        _thumbnailImageView.clipsToBounds = YES;
-        _thumbnailImageView.translatesAutoresizingMaskIntoConstraints = NO;
-        [self.contentView addSubview:_thumbnailImageView];
-
-        _titleLabel = [[UILabel alloc] init];
-        _titleLabel.font = [UIFont systemFontOfSize:16];
-        _titleLabel.numberOfLines = 2;
-        _titleLabel.translatesAutoresizingMaskIntoConstraints = NO;
-        [self.contentView addSubview:_titleLabel];
-
-        _statusLabel = [[UILabel alloc] init];
-        _statusLabel.font = [UIFont systemFontOfSize:12];
-        _statusLabel.textColor = [UIColor secondaryLabelColor];
-        _statusLabel.translatesAutoresizingMaskIntoConstraints = NO;
-        [self.contentView addSubview:_statusLabel];
-
-        _progressView = [[UIProgressView alloc] initWithProgressViewStyle:UIProgressViewStyleDefault];
-        _progressView.translatesAutoresizingMaskIntoConstraints = NO;
-        [self.contentView addSubview:_progressView];
-
-        [NSLayoutConstraint activateConstraints:@[
-            [_thumbnailImageView.leadingAnchor constraintEqualToAnchor:self.contentView.leadingAnchor constant:16],
-            [_thumbnailImageView.centerYAnchor constraintEqualToAnchor:self.contentView.centerYAnchor],
-            [_thumbnailImageView.widthAnchor constraintEqualToConstant:80],
-            [_thumbnailImageView.heightAnchor constraintEqualToConstant:45],
-
-            [_titleLabel.leadingAnchor constraintEqualToAnchor:_thumbnailImageView.trailingAnchor constant:12],
-            [_titleLabel.trailingAnchor constraintEqualToAnchor:self.contentView.trailingAnchor constant:-16],
-            [_titleLabel.topAnchor constraintEqualToAnchor:self.contentView.topAnchor constant:12],
-
-            [_statusLabel.leadingAnchor constraintEqualToAnchor:_titleLabel.leadingAnchor],
-            [_statusLabel.trailingAnchor constraintEqualToAnchor:_titleLabel.trailingAnchor],
-            [_statusLabel.topAnchor constraintEqualToAnchor:_titleLabel.bottomAnchor constant:4],
-
-            [_progressView.leadingAnchor constraintEqualToAnchor:_titleLabel.leadingAnchor],
-            [_progressView.trailingAnchor constraintEqualToAnchor:_titleLabel.trailingAnchor],
-            [_progressView.bottomAnchor constraintEqualToAnchor:self.contentView.bottomAnchor constant:-8],
-            [_progressView.heightAnchor constraintEqualToConstant:2],
-        ]];
-    }
-    return self;
-}
-
-@end
+#import "DownloadedCell.h"
 
 @interface DownloadedVC () <UITableViewDataSource, UITableViewDelegate>
 
--- Classes/UI/ViewControllers/DownloadingVC.m
+++ Classes/UI/ViewControllers/DownloadingVC.m
@@ -1,78 +1,7 @@
 #import "DownloadingVC.h"
 #import "DownloadsManager.h"
 #import "DownloadItem.h"
-
-@interface DownloadingCell : UITableViewCell
-@property (nonatomic, strong) UIImageView *thumbnailImageView;
-@property (nonatomic, strong) UILabel *titleLabel;
-@property (nonatomic, strong) UILabel *progressLabel;
-@property (nonatomic, strong) UIProgressView *progressView;
-@property (nonatomic, strong) UIButton *cancelButton;
-@end
-
-@implementation DownloadingCell
-
-- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
-    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
-    if (self) {
-        self.backgroundColor = [UIColor systemBackgroundColor];
-        self.contentView.backgroundColor = [UIColor systemBackgroundColor];
-
-        _thumbnailImageView = [[UIImageView alloc] init];
-        _thumbnailImageView.contentMode = UIViewContentModeScaleAspectFill;
-        _thumbnailImageView.clipsToBounds = YES;
-        _thumbnailImageView.translatesAutoresizingMaskIntoConstraints = NO;
-        [self.contentView addSubview:_thumbnailImageView];
-
-        _titleLabel = [[UILabel alloc] init];
-        _titleLabel.font = [UIFont systemFontOfSize:16];
-        _titleLabel.numberOfLines = 2;
-        _titleLabel.translatesAutoresizingMaskIntoConstraints = NO;
-        [self.contentView addSubview:_titleLabel];
-
-        _progressLabel = [[UILabel alloc] init];
-        _progressLabel.font = [UIFont systemFontOfSize:12];
-        _progressLabel.textColor = [UIColor secondaryLabelColor];
-        _progressLabel.translatesAutoresizingMaskIntoConstraints = NO;
-        [self.contentView addSubview:_progressLabel];
-
-        _progressView = [[UIProgressView alloc] initWithProgressViewStyle:UIProgressViewStyleDefault];
-        _progressView.translatesAutoresizingMaskIntoConstraints = NO;
-        [self.contentView addSubview:_progressView];
-
-        _cancelButton = [UIButton buttonWithType:UIButtonTypeSystem];
-        [_cancelButton setTitle:@"Cancel" forState:UIControlStateNormal];
-        _cancelButton.translatesAutoresizingMaskIntoConstraints = NO;
-        [self.contentView addSubview:_cancelButton];
-
-        [NSLayoutConstraint activateConstraints:@[
-            [_thumbnailImageView.leadingAnchor constraintEqualToAnchor:self.contentView.leadingAnchor constant:16],
-            [_thumbnailImageView.centerYAnchor constraintEqualToAnchor:self.contentView.centerYAnchor],
-            [_thumbnailImageView.widthAnchor constraintEqualToConstant:80],
-            [_thumbnailImageView.heightAnchor constraintEqualToConstant:45],
-
-            [_titleLabel.leadingAnchor constraintEqualToAnchor:_thumbnailImageView.trailingAnchor constant:12],
-            [_titleLabel.trailingAnchor constraintEqualToAnchor:_cancelButton.leadingAnchor constant:-8],
-            [_titleLabel.topAnchor constraintEqualToAnchor:self.contentView.topAnchor constant:12],
-
-            [_progressLabel.leadingAnchor constraintEqualToAnchor:_titleLabel.leadingAnchor],
-            [_progressLabel.topAnchor constraintEqualToAnchor:_titleLabel.bottomAnchor constant:4],
-
-            [_progressView.leadingAnchor constraintEqualToAnchor:_titleLabel.leadingAnchor],
-            [_progressView.trailingAnchor constraintEqualToAnchor:_cancelButton.leadingAnchor constant:-8],
-            [_progressView.bottomAnchor constraintEqualToAnchor:self.contentView.bottomAnchor constant:-8],
-            [_progressView.heightAnchor constraintEqualToConstant:2],
-
-            [_cancelButton.trailingAnchor constraintEqualToAnchor:self.contentView.trailingAnchor constant:-16],
-            [_cancelButton.centerYAnchor constraintEqualToAnchor:self.contentView.centerYAnchor],
-            [_cancelButton.widthAnchor constraintEqualToConstant:60],
-            [_cancelButton.heightAnchor constraintEqualToConstant:30],
-        ]];
-    }
-    return self;
-}
-
-@end
+#import "DownloadingCell.h"
 
 @interface DownloadingVC () <UITableViewDataSource, UITableViewDelegate, DownloadsManagerDelegate>
 
